465f8c681999de2c7ae1a070f9d2228c05cdaf49
Arpit Kapoor <kapoor.arpit97@gmail.com>
2024-06-04 14:24:35 +1000

    Notebook for QDeepGR4J-CNN

--- model/utils/evaluation.py
+++ model/utils/evaluation.py
@@ -8,21 +8,21 @@ def nse(targets: np.ndarray, predictions: np.ndarray):
 def normalize(x):
     return 1/(2 - x)
 
-def evaluate(P: np.ndarray, E: np.ndarray, Q: np.ndarray, Q_hat:np.ndarray, plot:bool = True):
+def evaluate(P: np.ndarray, E: np.ndarray, Q: np.ndarray, Q_hat:np.ndarray, quantiles: list, plot:bool = True):
 
     # Calculate NSE score
-    nse_score = nse(Q, Q_hat)
+    nse_score = nse(Q, Q_hat[:, int(len(quantiles)/2)])
     nnse_score = normalize(nse_score)
-
 
     # Plot hydrograph
     if plot:
         
         fig, ax = plt.subplots(figsize=(16, 6))
         ax.plot(Q, color='black', label='obs', alpha=1.0)
-        ax.plot(Q_hat, color='red', label='pred', alpha=0.75)
-        ax.plot(P, 'g--', label='precip', alpha=0.40)
-        ax.plot(E, 'y--', label='etp', alpha=0.30)
+        for i, q in enumerate(quantiles):
+            ax.plot(Q_hat[:, i], label=f'pred-{q:.2f}', alpha=0.75)
+        # ax.plot(P, 'g--', label='precip', alpha=0.40)
+        # ax.plot(E, 'y--', label='etp', alpha=0.30)
 
         ax.set_xlabel('Timestep')
         ax.set_ylabel('Flow (mm/day)')
